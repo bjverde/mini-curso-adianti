e0d79551613cd05ce76ba04b775ed5ecf35083ab
Bjverde <bjverde@yahoo.com.br>
2024-08-15 17:45:03 -0300

    config maindatabase

---
+++ appexemplo/app/config/maindatabase.php
@@ -0,0 +1,10 @@
+<?php 
+return[
+    'host' => "",
+    'name' => "app/database/maindatabase.db",
+    'user' => "",
+    'pass' => "",
+    'type' => "sqlite",
+    'prep' => "1",
+    'slog' => "SystemSqlLogService"
+];
